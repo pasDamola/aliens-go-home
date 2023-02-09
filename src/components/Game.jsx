211a774ecc6fe8dd3529e12f5f45c2fcfd2bebb0
Oyindamola Oyetunmibi <oyindamola.oyetunmibi@omnipresent.com>
2023-02-09 14:22:18 +0100

    removed the useSelector hook

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,6 @@
-import { useSelector } from "react-redux";
 import Canvas from "./Canvas";
 
 function Game() {
-  const message = useSelector((state) => state.message);
-
   return <Canvas />;
 }
 
