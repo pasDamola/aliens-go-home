9685860725b591c24b54c7f4fe0a15c1b984652e
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-02-09 14:39:25 +0100

    updates on the formula function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import App from "./App";
-import { reducer } from "./reducers";
+import reducer from "./reducers";
 
 const store = createStore(reducer);
 
